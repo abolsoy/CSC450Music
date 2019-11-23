eabf8571ada1006ff3cf268927ed26429ca2dc4e
Payton Weatherspoon <paytonweatherspoon@gmail.com>
2019-11-22 23:40:18 -0500

    Update album_Confirmation.php

--- GroupProj/album_Confirmation.php
+++ GroupProj/album_Confirmation.php
@@ -1,5 +1,5 @@
 <?php
-  if(!empty($_GET['artist_ID']) && !empty($_GET['album_Name']) && !empty($_GET['rec_Label']) && !empty($_GET['rel_Date']) && !empty($_GET['genre_ID'])) {
+    if(!empty($_GET['artist_ID']) && !empty($_GET['album_Name']) && !empty($_GET['rec_Label']) && !empty($_GET['rel_Date']) && !empty($_GET['genre_ID'])) {
     require_once('../../mysqli_config.php');
 
     $query1 = "SELECT MAX(album_ID) FROM Album";
@@ -59,13 +59,13 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <title>Album Added</title>
-	<meta charset ="utf-8">
-</head>
-<body>
-	<h2>The album "<?php echo "$album_name";?>" was successfully added</h2>
-	<h3><a href="insert_Album_Info.php">Add another album</a><h3>
-	<h3><a href="index.html">Back to Home</a></h3>
-</body>
+      <meta charset ="utf-8">
+  </head>
+  <body>
+    <h2>The album "<?php echo "$album_name";?>" was successfully added</h2>
+    <h3><a href="insert_Album_Info.php">Add another album</a><h3>
+      <h3><a href="index.html">Back to Home</a></h3>
+    </body>
 </html>
